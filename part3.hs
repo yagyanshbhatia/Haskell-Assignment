af8a94884c736675f66b4f7045666cfb84589495
yagyanshbhatia <yagyansh1999@gmail.com>
2019-11-18 01:45:50 +0530

    Bug fixes.

--- part3.hs
+++ part3.hs
@@ -2,57 +2,61 @@ import Data.List
 import System.IO
 import System.Random
 import Data.Maybe
+import System.IO.Unsafe
 
-allTeams = ['BS','CM','CH','CV','CS','DS','EE','HU','MA','ME','PH','ST']
+allTeams = ["BS","CM","CH","CV","CS","DS","EE","HU","MA","ME","PH","ST"]
 newTeamList = []
 
 getRandomIndex :: Int -> Int
-getRandomIndex b = randomRIO (0,b)
+getRandomIndex b = unsafePerformIO (getStdRandom (randomR (0, b)))
 
 -- elemIndex :: Eq a => a -> [a] -> Maybe Int
 
 removeItem :: String -> [String] -> [String]
 removeItem _ []                 = []
-removeItem x (y:ys) | x == y    = removeItem x ys
+removeItem x (y:ys) | x == y    = ys
                     | otherwise = y : removeItem x ys
 
-generateNewTeamList :: Int -> [String]
-generateNewTeamList 0 = return newTeamList
-generateNewTeamList n =
-	do
-		randomIndex = getRandomIndex(n-1)
-		teamId = allTeams !! randomIndex
-		newTeamList = newTeamList ++ teamId
-		allTeams = removeItem teamId
-		generateNewTeamList (n-1)
+generateNewTeamList :: Int -> [String] -> [String]
+generateNewTeamList 0 _ = return [ ]
+generateNewTeamList n allTeams = teamId : generateNewTeamList (n-1) y
+    where
+        teamId = allTeams !! randomIndex
+        y = removeItem teamId allTeams
+        randomIndex = getRandomIndex(n-1)
 
-generateFixture :: Int
-generateFixture 0 = return ()
-generateFixture n = 
-	do
-		startIndex = (n-6)*2
-		team1 = newTeamList !! startIndex
-		team2 = newTeamList !! (startIndex+1)
-		if (n-6) 'rem' 2 == 0
-			then time = ' 9:30'
-		else time = ' 19:30'
-		date = (n-6)/2 + 1
-		putStrLn team1 + ' vs ' + team2 + ' ' + date + ' ' + time
-		generateFixture (n-1)
+generateFixture :: Int -> [[Char]] -> IO ()
+generateFixture 0 _ = return ()
+generateFixture n newTeamList = 
+    do
+        let startIndex = (6-n)*2
+        let team1 = newTeamList !! startIndex
+        let team2 = newTeamList !! (startIndex+1)
+        let date = ((6-n) `div` 2) + 1
+        if (n-6) `rem` 2 == 0
+            then putStrLn (team1 ++ " vs " ++ team2 ++ " " ++ (show date) ++ " " ++ " 9:30")
+        else putStrLn (team1 ++ " vs " ++ team2 ++ " " ++ (show date) ++ " " ++ " 19:30")
+        generateFixture (n-1) newTeamList
 
-fixture :: String
-fixture 'all' = generateFixture 6
-fixture team1 = 
-	do
-		teamIndex = elemIndex team1 newTeamList
-		if teamIndex 'rem' 2 == 0
-			then team2 = newTeamList !! (teamIndex+1)
-		else team2 = newTeamList !! (teamIndex-1)
-		date = (teamIndex/4) + 1
-		teamIndex = teamIndex - (teamIndex/4)*4
-		if teamIndex/2 == 0
-			then time = ' 9:30'
-		else time = ' 19:30'
-		putStrLn = team1 + ' vs ' + team2 + ' ' + date + ' ' + time
+-- fixture :: [Char] -> IO ()
+-- fixture "all" = generateFixture 6 $ init $ generateNewTeamList 12 allTeams
+-- fixture team1 = 
+-- 	do
+-- 		teamIndex = elemIndex team1 newTeamList
+-- 		if teamIndex `rem` 2 == 0
+-- 			then team2 = newTeamList !! (teamIndex+1)
+-- 		else team2 = newTeamList !! (teamIndex-1)
+-- 		date = (teamIndex/4) + 1
+-- 		teamIndex = teamIndex - (teamIndex/4)*4
+-- 		if teamIndex/2 == 0
+-- 			then time = " 9:30"
+-- 		else time = " 19:30"
+-- 		putStrLn team1 ++ " vs " ++ team2 ++ " " ++ (show date) ++ " " ++ time
 
 -- nextMatch ::
+
+
+
+
+
+
