7dcb9609c71edc4bb34c7b1ec3367aef9410d310
varunkedia <varunkedia4@gmail.com>
2019-11-19 20:35:18 +0530

    1,2,3 Done.

--- part1a.hs
+++ part1a.hs
@@ -1,6 +1,8 @@
+-- Function to convert 2D list to 1D-internally summed list
 innerSum :: [[Int]] -> [Int]
 innerSum = map sum
 
-
+-- Function to find product of summed-up lists
 innerProd :: [[Int]] -> Int
+innerProd [] = 0
 innerProd x = product (innerSum x)
